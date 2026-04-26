9ff4932d5eb807bf4c411cdfc8554ddcaf07a07f
Anson Lim <anson252093@dasein.edu.my>
2026-04-26 21:58:08 +0800

    feat:added side menu

---
+++ web/css/js/header.js
@@ -0,0 +1,12 @@
+const toggleButton = document.getElementById("bento_menu");
+const menu = document.getElementById("headerside_menu");
+
+toggleButton.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    // Change icon
+    if (menu.classList.contains("active")) {
+        toggleButton.innerHTML = "&times;"; // × (cross)
+    } else {
+        toggleButton.innerHTML = '<img src="../../img/bento-menu.svg" alt="bento_menu" width="20px">'; // ☰ (hamburger)
+    }
+});
